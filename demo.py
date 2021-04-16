b05d1ad7cec0979f26e55b01540ad4c30a1130e8
5456465tony97202 <109021079@live.asia.edu.tw>
2021-04-16 16:04:54 +0800

    0

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 import sys
 import requests
+import time
 from bs4 import BeautifulSoup
 
 URL="https://search.books.com.tw/search/query/key/{0}/cat/all"
@@ -22,19 +23,46 @@ def parse_html(html_str):
         soup=None
     return soup
 
+
 def web_scraping_bot(url):
     booklist=[]
     print("retrive data form Internet...")
     soup=parse_html(get_resource(url))
-    print(soup)
+    if soup != None:
+       tag_item=soup.find_all(class_="box_1") #找尋
+       for item in tag_item:
+           book=[]
+           book.append(item.find("img")["alt"]) #書名
+           [isbn,price]=get_ISBN_price(item.find("a")["href"])
+           book.append(isbn)
+           book.append(price)
+
+           print(book)
+           print("wait 2 sec")
+           time.sleep(2)
+
+def get_ISBN_price(url):
+    url1="https:"+url
+    soup=parse_html(get_resource(url1))
+    isbnStr=""
+    if soup!=None:
+       bd=soup.find(class_="bd")
+       lilist=bd.find(class_="lilist")
+       print("lilist \n",lilist)
+       price=0
+       priceU1=soup.find("ul",{"class":"price"})
+       for liData in lilist:
+           print("liData\n\n",liData.text)
+           if "ISBN" in liData.text
+
 
 if __name__=="__main__":
     if len(sys.argv)>1:
        url=generate_search_url(URL,sys.argv[1])
        r=get_resource(url)
        booklist=web_scraping_bot(url)
-       for item in booklist:
-           print(item)
+       #for item in booklist:
+           #print(item)
 
 
 
